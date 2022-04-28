df071fca760f616954c61c8a8eb118941ebccade
drewmullen <drmullen@amazon.com>
2022-04-28 08:13:31 -0400

    rm embedded provider

--- versions.tf
+++ versions.tf
@@ -10,11 +10,3 @@ terraform {
     }
   }
 }
-
-provider "awscc" {
-  user_agent = [{
-    product_name    = "terraform-awscc-label"
-    product_version = "0.0.4"
-    comment         = "V1/AWS-D69B4015/376222271"
-  }]
-}
